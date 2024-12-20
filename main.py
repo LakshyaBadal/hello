8498c323b5c151fcfa08c87f8be9681a3e2ef0ea
LakshyaBadal <lakshyabadal12027@outlook.com>
2024-12-20 18:53:57 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+def circle(text: str, text2: str, text3: str):
+    game.splash(text)
+circle("diameter", "radius", "chord")
---
+++ main.ts
@@ -0,0 +1,10 @@
+function circle (text: string, text2: string) {
+    aalu = text
+    if (aalu == text) {
+        game.splash(text)
+    } else {
+        game.splash(text2)
+    }
+}
+let aalu = ""
+circle("abc", "radius")
